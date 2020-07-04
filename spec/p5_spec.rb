cd040f87b1c655c3c2e11571f698e02bb770a8f6
Karmele <alu0100966930@ull.edu.es>
2020-07-04 18:12:52 +0100

    Faltan los dos ultimos puntos de la 6

--- spec/p5_spec.rb
+++ spec/p5_spec.rb
@@ -14,5 +14,17 @@ RSpec.describe P5 do
 	  it "Terreno utilizado al año" do
 		  expect(@carne_vaca.t).to eq(164.0)
 	  end
+	  it "Método para el nombre" do
+		  expect(@carne_vaca.get_name).to eq("carne de vaca")
+	  end
+	  it "Método para las emisiones de gases" do 
+		  expect(@carne_vaca.get_gei).to eq(50.0)
+	  end
+	  it "Método para el terreno" do 
+		  expect(@carne_vaca.get_t).to eq(164.0)
+	  end
+	  it "Valor energético" do 
+		  expect(@carne_vaca.val_energetico).not_to eq(nil)
+	  end
   end
 end
