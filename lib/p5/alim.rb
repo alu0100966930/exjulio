2a6a13588db18de6b72b2376bd86da2405e35caf
Karmele <alu0100966930@ull.edu.es>
2020-07-04 19:59:19 +0100

    p7 menos ultimo ejercicio

--- lib/p5/alim.rb
+++ lib/p5/alim.rb
@@ -26,7 +26,53 @@ class Alimento
 
 	def val_energetico
 		(@ch*4)+(@l*9)+(@p*4)
+	end	
+end
+
+Node = Struct.new(:value, :next, :prev)
+class List
+
+	attr_accessor :value, :next, :prev, :head, :tail, :size
+	def initialize
+		@head = Node.new
+		@tail = Node.new
+		@size = 0
 	end
 
-	
-end
+	def push(n)
+		if(@size == 0)
+			@tail = n
+			@tail.next = nil
+		else
+			@head.prev = n
+			n.next = @head
+		end
+		@head = n
+		n.prev = nil
+		@size = @size + 1
+	end
+
+	def pop_head
+		if(@size == 0)
+			puts "Lista vacía"
+		else
+			drop = @head.value
+			(@head.next).prev = nil
+			@head = @head.next
+			@size = @size - 1
+			return drop
+		end
+	end
+	def pop_tail
+		if(@size == 0)
+			puts "Lista vacía"
+		else
+			drop = @tail.value
+			(@tail.prev).next = nil
+			@tail = @tail.prev
+			@size = @size - 1
+			return drop
+		end
+	end
+
+end		
